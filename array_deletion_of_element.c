131a7573c10953bdf59fc06964e3e971eee32aba
Meghana2004 <120378127+Meghana-200@users.noreply.github.com>
2023-09-26 12:01:05 +0530

    Add files via upload

---
+++ array_deletion_of_element.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+int a[100],i,pos,item,n;
+printf("Enter array size: ");
+scanf("%d",&n);
+printf("Enter array elements: ");
+for(i=0;i<=n-1;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n-1;i++)
+{
+printf("%d\n",a[i]);
+}
+n=n+1;
+printf("Enter the pos: ");
+scanf("%d",&pos);
+printf("Enter the item: ");
+scanf("%d",&item);
+for(i=pos-1;i<n-1;i++)
+{
+a[i]=a[i+1];
+}
+n=n-1;
+for(i=0;i<n-1;i++)
+{
+printf("%d\n",a[i]);
+}
+return;
+}
---
+++ read_and_write_arrays.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a[5],i;
+printf("Enter array elements: ");
+for(i=0;i<=4;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=4;i++)
+{
+printf("%d\n",a[i]);
+}
+}
